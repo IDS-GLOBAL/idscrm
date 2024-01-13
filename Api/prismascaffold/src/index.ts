57634f261f1108b99e6b6aaf3a2d8f8e226c1c71
Webgoonie <webgoonie@gmail.com>
2024-01-13 10:44:55 -0500

    Created The Minimal Typescript Packages To Use The Results Of Prisma Output

--- Api/prismascaffold/src/index.ts
+++ Api/prismascaffold/src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello TypeScript We Should Be Working Quite Well')
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+const PORT = 8081
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`listening on server listening on port ${PORT} http://localhost:${PORT}`);
+})

a6ab2fa6fef8b689b48de3466377f18e20f5d21d
Webgoonie <webgoonie@gmail.com>
2024-01-24 12:46:00 -0500

    Added Legacy images and js folders from old project

---
+++ Legacy/js/cufon-replace.js
@@ -0,0 +1,6 @@
+Cufon.replace('#header .nav a', { fontFamily: 'Helvetica-Condensed-Black-Se', textShadow: '#000 1px 1px', hover:true });
+Cufon.replace('h1', { fontFamily: 'Helvetica-Condensed-Black-Se'});
+Cufon.replace('h2.color1', { fontFamily: 'Helvetica-Condensed-Black-Se', textShadow: '#fff 1px 1px'});
+Cufon.replace('h2.color2', { fontFamily: 'Helvetica-Condensed-Black-Se', textShadow: '#0945a3 1px 1px'});
+Cufon.replace('h2.color3', { fontFamily: 'Helvetica-Condensed-Black-Se', textShadow: '#586168 1px 1px'});
+Cufon.replace('h3', { fontFamily: 'Helvetica-Condensed-Black-Se', textShadow: '#525b62 1px 1px'});
---
+++ Legacy/js/home.js
@@ -0,0 +1,10 @@
+// Custom scripts
+$(document).ready(function(){
+						   
+
+			
+			//alert('I am working');
+								   
+
+});
+

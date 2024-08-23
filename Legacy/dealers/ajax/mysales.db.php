e33f42128f3050cfc5da9485183480cdd6a5ff0a
Webgoonie <webgoonie@gmail.com>
2024-08-23 15:50:23 -0400

    updated db connections in ajax db connections

--- Legacy/dealers/ajax/mysales.db.php
+++ Legacy/dealers/ajax/mysales.db.php
@@ -1,5 +1,5 @@
-<?php require_once('../Connections/idsconnection.php'); ?>
-<?php require_once('../Connections/tracking.php'); ?>
+<?php require_once('../../Connections/idsconnection.php'); ?>
+<?php require_once('../../Connections/tracking.php'); ?>
 <?php
 
 

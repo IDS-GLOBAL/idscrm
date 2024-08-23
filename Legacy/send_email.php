313010e88961550803812357e93893a1433941c5
Webgoonie <webgoonie@gmail.com>
2024-08-23 01:40:52 -0400

    Email From Home Page Works As Expected

--- Legacy/send_email.php
+++ Legacy/send_email.php
@@ -37,9 +37,7 @@ try {
 
     $mail->send();
     echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+
 
 
     //Recipients
